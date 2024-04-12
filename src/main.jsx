1b447907709d7014528c3e46b80592c9821deea3
tsheporamantso <tshepo.ramantso@outlook.com>
2024-04-12 09:21:41 +0200

    Create entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-import App from './App.jsx';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const el = document.getElementById('root')
+const root = ReactDOM.createRoot(el)
+root.render(<App />)
