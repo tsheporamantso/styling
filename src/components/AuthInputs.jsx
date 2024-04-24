fb3878bea6c1cef933accbdd3ad0d6e1e7736d7e
tsheporamantso <tshepo.ramantso@outlook.com>
2024-04-24 14:28:12 +0200

    Add module css class and scope Header paragraph className

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -23,7 +23,7 @@ export default function AuthInputs() {
   return (
     <div id="auth-inputs">
       <div className="controls">
-        <p>
+        <p className="paragraph">
           <label>Email</label>
           <input
             type="email"
