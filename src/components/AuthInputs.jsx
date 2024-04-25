0ee04d44e575233baee71a346417e06d07c99676
tsheporamantso <tshepo.ramantso@outlook.com>
2024-04-25 12:08:03 +0200

    Add butoon component on folder structure

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import styled from 'styled-components';
+import Button from './Button';
 
 const ControlContainer = styled.div`
   display: flex;
@@ -28,21 +29,6 @@ const Input = styled.input`
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `
-
-const Button = styled.button`
-  padding: 1rem 2rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  border-radius: 0.25rem;
-  color: #1f2937;
-  background-color: #f0b322;
-  border-radius: 6px;
-  border: none;
-  
-  &:hover {
-    background-color: #f0920e;
-  }
-`;
 
 const TextButton = styled.button`
   color: #f0b322;
