f40038db62fab5193741562b3aa98c63235ff730
tsheporamantso <tshepo.ramantso@outlook.com>
2024-04-25 14:06:15 +0200

    Add tailwind media query

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -47,9 +47,9 @@ import { styled } from 'styled-components';
 
 export default function Header() {
   return (
-    <header className="flex flex-col items-center mt-8 mb-16">
+    <header className="flex flex-col items-center mt-8 mb-8 md:mb-16">
       <img className="mb-8 w-44 h-44 object-contain" src={logo} alt="A canvas" />
-      <h1 className="text-4xl text-cente tracking-widest font-semibold uppercase text-amber-800 font-title">ReactArt</h1>
+      <h1 className=" text-xl md:text-4xl text-cente tracking-widest font-semibold uppercase text-amber-800 font-title">ReactArt</h1>
       <p className="text-center text-stone-500">A community of artists and art-lovers.</p>
     </header>
   );
